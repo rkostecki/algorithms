e59740a3c86298ed6622f03754c7de6f5fe3f070
rkostecki <r.kostecki@sportradar.com>
2018-08-24 11:34:04 +0200

    random numbers generator

--- js/sort/insertionSort.spec.js
+++ js/sort/insertionSort.spec.js
@@ -1,5 +1,6 @@
 const insertionSort = require('./insertionSort');
 const {expect} = require('chai');
+const crypto = require('crypto');
 
 describe('InsertionSort tests', () => {
     it('sorts empty array', () => {
@@ -30,9 +31,49 @@ describe('InsertionSort tests', () => {
         insertionSort(array);
         expect(array).to.deep.equal([1, 2]);
     });
+
+    it('generate 10000 numbers array', () => {
+
+        const res = Array.from(arrayIterator(10000));
+        expect(isSorted(res)).to.be.false;
+        insertionSort(res);
+        expect(isSorted(res)).to.be.true;
+    });
 });
 
-function generateArray (max) {
-    const arr = [];
-    for(i = 0; )
+function isSorted (array) {
+    let prev = null;
+    if (array.length === 1) {return true;}
+
+    return !array.some(val => {
+        const res = (prev !== null && prev > val);
+        prev = val;
+
+        return res;
+    });
+}
+
+function arrayIterator (max) {
+    let index = 0;
+
+    return {
+        [Symbol.iterator]() {
+            const iterator = {
+                next () {
+                    if (index < max) {
+                        index++;
+                        return { done: false, value: randomInt() };
+                    } else {
+                        return { done: true, value: null };
+                    }
+                }
+            };
+            return iterator;
+        }
+    };
+}
+
+function randomInt() {
+    return parseInt(crypto.randomBytes(4).toString('hex'), 16);
+
 }
