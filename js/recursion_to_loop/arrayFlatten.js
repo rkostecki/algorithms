f5d6c86790142c16a53f4545f65798932898d449
Rafal Kostecki <praca@rafalkostecki.pl>
2018-09-18 22:21:36 +0200

    arrayFlattenLoop

--- js/recursion_to_loop/arrayFlatten.js
+++ js/recursion_to_loop/arrayFlatten.js
@@ -11,7 +11,34 @@ function arrayFlattenJS (arr) {
     return JSON.stringify(arr).replace(/\[|\]/g, '').split(',').map(JSON.parse);
 }
 
+function arrayFlattenApply (arr) {
+    return Array.prototype.concat.apply([], arr);
+}
+
+function arrayFlattenLoop (arr) {
+    const len = arr.length;
+    const res = [];
+    let i = 0;
+    while(true) {
+        if (i > len-1) {
+            break;
+        }
+        const value = arr[i];
+        if (!Array.isArray(value)) {
+           res.push(value);
+        } else {
+            //
+            res.concat(value);
+        }
+        i++;
+
+    }
+    return res;
+}
+
 module.exports = {
     arrayFlatten,
-    arrayFlattenJS
+    arrayFlattenJS,
+    arrayFlattenApply,
+    arrayFlattenLoop
 };
--- js/recursion_to_loop/arrayFlatten.spec.js
+++ js/recursion_to_loop/arrayFlatten.spec.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai');
-const {arrayFlatten, arrayFlattenJS} = require('./arrayFlatten');
+const {arrayFlatten, arrayFlattenJS, arrayFlattenApply, arrayFlattenLoop} = require('./arrayFlatten');
 
 describe('arrayFlatten tests', () => {
     it('arrayFlatten recursive', () => {
@@ -10,9 +10,21 @@ describe('arrayFlatten tests', () => {
         expect(resArr).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
     });
     it('arrayFlatten join, split', () => {
-        const array = [1, [2, 3], [4, 5, [6,7]]];
+        const array = [1, [2,[101, -102, {a: 1}], 3], [4, 5, [6,7]]];
         const res = arrayFlattenJS(array);
 
-        expect(res).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
+        expect(res).to.deep.equal([1, 2, 101, -102, {a: 1}, 3, 4, 5, 6,7]);
     });
-});
+    it('arrayFlatten apply one level only', () => {
+        const array = [1, [2, 101, -102, {a: 1}, 3], [4, 5, 6,7]];
+        const res = arrayFlattenApply(array);
+
+        expect(res).to.deep.equal([1, 2, 101, -102, {a: 1}, 3, 4, 5, 6,7]);
+    });
+    it('arrayFlatten while', () => {
+        const array = [1, [2,[101, -102, {a: 1}], 3], [4, 5, [6,7]]];
+        const res = arrayFlattenLoop(array);
+
+        expect(res).to.deep.equal([1, 2, 101, -102, {a: 1}, 3, 4, 5, 6,7]);
+    });
+ });
